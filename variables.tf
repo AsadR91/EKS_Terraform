5e7ab6e9a52a29ca476742d1350ddaf85ce922bc
AsadR91 <130393804+AsadR91@users.noreply.github.com>
2025-05-06 17:37:33 -0400

    Initial Commit
    Original Code was acquired by Aditya Jaiswal

---
+++ variables.tf
@@ -0,0 +1,53 @@
+# This is to make code DRY
+
+variable "region" {
+  description = "AWS region to deploy to"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "ssh_key_name" {
+  description = "The name of the SSH key pair to use for instances"
+  type        = string
+  default     = "Project-key"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_count" {
+  description = "Number of subnets to create"
+  type        = number
+  default     = 2
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "node_instance_type" {
+  description = "EC2 instance type for EKS worker nodes"
+  type        = string
+  default     = "t2.medium"
+}
+
+variable "desired_capacity" {
+  description = "Desired number of worker nodes"
+  type        = number
+  default     = 3
+}
+
+
+
+
+# variable "ssh_key_name" {
+#   description = "The name of the SSH key pair to use for instances"
+#   type        = string
+#   default     = "Project-key"
+# }
+#
