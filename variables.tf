77af448127ad767698054008051a3dde1acce398
Asad Rafique <130393804+AsadR91@users.noreply.github.com>
2025-06-17 21:40:14 -0400

    Keeping code DRY

--- variables.tf
+++ variables.tf
@@ -1,18 +1,4 @@
-output "cluster_id" {
-  value = aws_eks_cluster.main.id
-}
-
-output "node_group_id" {
-  value = aws_eks_node_group.main.id
-}
-
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "subnet_ids" {
-  value = aws_subnet.main[*].id
-}variable "region" {
+variable "region" {
   description = "AWS region to deploy to"
   type        = string
   default     = "us-east-1"
